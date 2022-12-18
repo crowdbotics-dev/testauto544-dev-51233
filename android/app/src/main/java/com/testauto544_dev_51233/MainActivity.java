45e541378d5a1e2478f607348be00753ab519642
Crowdbotics <engineering@crowdbotics.com>
2022-12-18 00:57:34 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/testauto544_dev_51233/MainActivity.java
@@ -0,0 +1,15 @@
+package com.testauto544_dev_51233;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "testauto544_dev_51233";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "testauto544-dev-51233",
+  url: "https://testauto544-dev-51233.botics.co",
+  api: "https://testauto544-dev-51233.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
